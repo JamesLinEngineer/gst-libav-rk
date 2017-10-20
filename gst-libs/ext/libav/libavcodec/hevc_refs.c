9aec270eea59040cdfefdd1bef3daec0d0420700
james.lin <james.lin@rock-chips.com>
2017-10-20 16:46:36 +0800

    libav: hevc allow hwaccel generate few missing ref
    In order to save memory close hwaccel generate missing ref. But
    few stream can not play because linkage reference. In order to
    balance memory save and play compatibility, allow hwaccel generate
    few(only one just now) missing ref.
    
    Change-Id: I4f5744e155662c2f0cfd81ce0d07b6a3922e2d62
    Signed-off-by: james.lin <james.lin@rock-chips.com>

--- gst-libs/ext/libav/libavcodec/hevc_refs.c
+++ gst-libs/ext/libav/libavcodec/hevc_refs.c
@@ -443,7 +443,7 @@ static int add_candidate_ref(HEVCContext *s, RefPicList *list,
         return AVERROR_INVALIDDATA;
 
     if (!ref) {
-        if (s->avctx->hwaccel)
+        if (s->avctx->hwaccel && list->nb_refs > 1)
             return AVERROR_INVALIDDATA;
         ref = generate_missing_ref(s, poc);
         if (!ref)
